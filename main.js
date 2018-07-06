335c8371509d1844150e1d2d7ffe71b5d0de8ca5
Talytta Carvalho <talytta110@hotmail.com>
2018-07-06 17:36:39 -0300

    versao 0.0.2 em progresso

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var newText = document.querySelector(".message-box");
 var newTweets = document.querySelector(".tweets");
 var button = document.querySelector(".btn");
+var span = document.querySelector("span");
+
+var char = 140;
+
 
 function addTweet() {
   var novaDiv = document.createElement("div");
@@ -10,6 +14,29 @@ function addTweet() {
   newTweets.prepend(novaDiv);
   novaDiv.appendChild(novoPar);
   newText.value = "";
+  disableBtn();
+}
+
+function verifyContent() {
+  if(newText.value){
+    button.removeAttribute("disabled");
+  }
+  char--;
+  span.textContent = char;
 }
 
 button.addEventListener("click", addTweet);
+newText.addEventListener("keypress", verifyContent);
+
+function disableBtn() {
+  if (!newText.value) {
+    button.setAttribute("disabled",true);
+  } 
+}
+
+function loadpage(){
+  disableBtn();
+  span.textContent = char;
+}
+
+window.onload = loadpage;
