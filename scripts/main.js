ccd0c5cbefb14680df9c9ac3b5847570df5da02f
Talytta Carvalho <talytta110@hotmail.com>
2018-07-09 16:42:31 -0300

    versao 0.0.6

--- scripts/main.js
+++ scripts/main.js
@@ -2,32 +2,19 @@ var newText = document.querySelector(".message-box");
 var newTweets = document.querySelector(".tweets");
 var button = document.querySelector(".btn");
 var contNumber = document.querySelector(".contNumber");
-var span = document.querySelector("span");
 var char = 140;
-var counter = 0;
 var numChar;
 
-
-function addTweet() {
-  var novaDiv = document.createElement("div");
-  var novoPar = document.createElement("p");
-  novoPar.textContent = newText.value;
-  novaDiv.setAttribute("class", "posts");
-  newTweets.prepend(novaDiv);
-  novaDiv.appendChild(novoPar);
-  newText.value = "";
-  contNumber.textContent = char;
-}
-button.addEventListener("click", addTweet);
-
-function charScore() {
+function textLength() {
   numChar = newText.value.length;
   if (numChar) {
     contNumber.textContent = char - numChar;
   } else {
     contNumber.textContent = char;
   }
+}
 
+function textColorLength() {
   if (numChar >= 120 && numChar < 130) {
     contNumber.style.color = "orange";
   } else if (numChar >= 130 && numChar < 140) {
@@ -37,17 +24,47 @@ function charScore() {
   } else if (numChar < 120) {
     contNumber.style.color = "#3991F5";
   }
+}
 
-  if (newText.value !== "" && numChar <= 140) {
+function disableBtn() {
+  if (newText.value.trim() !== "" && numChar <= 140) {
     button.removeAttribute("disabled", "false");
-  } else if (newText.value === "" || numChar > 140) {
+  } else if (newText.value.trim() === "" || numChar > 140) {
     button.setAttribute("disabled", "true");
   }
+}
 
+function autoResize() {
   newText.style.height = "40px";
   newText.style.height = newText.scrollHeight + "px";
 }
-newText.addEventListener("keypress", charScore);
-newText.addEventListener("keyup", charScore);
-newText.addEventListener("keydown", charScore);
 
+function addTweet() {
+  var novaDiv = document.createElement("div");
+  var novoPar = document.createElement("p");
+  var horario = document.createElement("p");
+  novoPar.textContent = newText.value;
+  horario.textContent = moment().format('L') + ' ' + moment().format('LT');
+  novaDiv.setAttribute("class", "posts");
+  newTweets.prepend(novaDiv);
+  novaDiv.appendChild(novoPar);
+  horario.setAttribute('class', 'hours')
+  novaDiv.appendChild(horario);
+  newText.value = "";
+  contNumber.textContent = char;
+  disableBtn();
+  autoResize()
+
+}
+
+function textTweet() {
+  textLength();
+  textColorLength();
+  disableBtn();
+  autoResize();
+}
+
+newText.addEventListener("keypress", textTweet);
+newText.addEventListener("keyup", textTweet);
+newText.addEventListener("keydown", textTweet);
+button.addEventListener("click", addTweet);
